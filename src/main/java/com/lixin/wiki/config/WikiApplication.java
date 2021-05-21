2b0166fa2df5b23ddf2d17f9611d6e093476f168
MaoLixin1998 <maoxin98116@163.com>
2021-05-21 17:00:33 +0800

    2-6 修改主类扫描路径

--- src/main/java/com/lixin/wiki/config/WikiApplication.java
+++ src/main/java/com/lixin/wiki/config/WikiApplication.java
@@ -1,14 +1,17 @@
-package com.lixin.wiki;
+package com.lixin.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 /**
  * @author mao
  */
+//@ComponentScan({"com.lixin", "com.test"})
+@ComponentScan("com.lixin")
 @SpringBootApplication
 public class WikiApplication {
 
