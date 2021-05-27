00e2df183be5926b03cd08b9137e08c220071238
MaoLixin1998 <maoxin98116@163.com>
2021-05-27 13:56:28 +0800

    3-5 测试mybatis-generator代码生成器

--- src/main/java/com/lixin/wiki/controller/DemoController.java
+++ src/main/java/com/lixin/wiki/controller/DemoController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import java.util.List;
 
+/**
+ * @author mao
+ */
 @RestController
 @RequestMapping(value = "/demo")
 public class DemoController {
