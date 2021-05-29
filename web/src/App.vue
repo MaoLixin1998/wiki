b54b2149a0ff8567ee47ee0a6aba85b9ecec8cbc
MaoLixin1998 <maoxin98116@163.com>
2021-05-29 10:25:42 +0800

    4-5 网站首页布局开发

--- web/src/App.vue
+++ web/src/App.vue
@@ -74,22 +74,15 @@
           </a-sub-menu>
         </a-menu>
       </a-layout-sider>
-      <a-layout style="padding: 0 24px 24px">
-        <a-breadcrumb style="margin: 16px 0">
-          <a-breadcrumb-item>Home</a-breadcrumb-item>
-          <a-breadcrumb-item>List</a-breadcrumb-item>
-          <a-breadcrumb-item>App</a-breadcrumb-item>
-        </a-breadcrumb>
-        <a-layout-content
-            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-        >
-          Content
-        </a-layout-content>
-        <a-layout-footer style="text-align: center">
-          Ant Design ©2018 Created by Ant UED
-        </a-layout-footer>
-      </a-layout>
+      <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+      >
+        Content
+      </a-layout-content>
     </a-layout>
+    <a-layout-footer style="text-align: center">
+      利鑫wiki知识分享
+    </a-layout-footer>
   </a-layout>
 </template>
 
