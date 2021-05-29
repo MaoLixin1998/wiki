288a20a4e8ee42fc7c1338be5bd50dc78ffd6b9f
MaoLixin1998 <maoxin98116@163.com>
2021-05-29 14:58:16 +0800

    4-6 将header抽出组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,24 +1,6 @@
 <template>
   <a-layout id="components-layout-demo-top-side-2">
-    <a-layout-header class="header">
-      <div class="logo" />
-      <a-menu
-          theme="dark"
-          mode="horizontal"
-          :default-selected-keys="['2']"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">
-          nav 1
-        </a-menu-item>
-        <a-menu-item key="2">
-          nav 2
-        </a-menu-item>
-        <a-menu-item key="3">
-          nav 3
-        </a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
       <router-view/>
     <a-layout-footer style="text-align: center">
       利鑫wiki知识分享
@@ -26,14 +8,16 @@
   </a-layout>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      collapsed: false,
-    };
+<script lang="ts">
+import {defineComponent} from 'vue';
+import TheHeader from '@/components/the-header.vue';
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    TheHeader,
   },
-};
+});
 </script>
 
 
