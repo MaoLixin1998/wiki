c8b2b474c13ea8ad225763e14e1faad28f7ac203
MaoLixin1998 <maoxin98116@163.com>
2021-06-02 11:14:47 +0800

    5-4 修改图表组件版本5.1.9

--- web/src/main.ts
+++ web/src/main.ts
@@ -9,7 +9,7 @@ import * as Icons from '@ant-design/icons-vue';
 const app = createApp(App);
 app.use(store).use(router).use(AntD).mount('#app')
 
-//全局使用图标
+// 全局使用图标
 const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
