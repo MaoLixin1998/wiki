4c2de1b1328854112bbb1ec8ddbaf327515cbdee
MaoLixin1998 <maoxin98116@163.com>
2021-06-07 13:53:44 +0800

    7-7 主页左侧菜单栏改为分类的层级菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -4,57 +4,21 @@
       <a-menu
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-          <span> <user-outlined />subnavasas 1</span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span> <user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">
-            option1
-          </a-menu-item>
-          <a-menu-item key="2">
-            option2
-          </a-menu-item>
-          <a-menu-item key="3">
-            option3
-          </a-menu-item>
-          <a-menu-item key="4">
-            option4
-          </a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-            <span> <laptop-outlined />subnavasas 2</span>
-          </template>
-          <a-menu-item key="5">
-            option5
-          </a-menu-item>
-          <a-menu-item key="6">
-            option6
-          </a-menu-item>
-          <a-menu-item key="7">
-            option7
-          </a-menu-item>
-          <a-menu-item key="8">
-            option8
-          </a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-
-          <template #title>
-            <span> <notification-outlined />subnavasas 3</span>
-          </template>
-          <a-menu-item key="9">
-            option9
-          </a-menu-item>
-          <a-menu-item key="10">
-            option10
-          </a-menu-item>
-          <a-menu-item key="11">
-            option11
-          </a-menu-item>
-          <a-menu-item key="12">
-            option12
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span>{{child.name}}</span>
           </a-menu-item>
         </a-sub-menu>
       </a-menu>
@@ -67,7 +31,7 @@
           <a-list-item key="item.name">
             <template #actions>
               <span v-for="{ type, text } in actions" :key="type">
-                <component v-bind:is="type" style="margin-right: 8px" />
+                <component v-bind:is="type" style="margin-right: 8px"/>
                 {{ text }}
               </span>
             </template>
@@ -75,7 +39,9 @@
               <template #title>
                 <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
             </a-list-item-meta>
             {{ item.content }}
           </a-list-item>
@@ -86,8 +52,10 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, reactive, ref, toRef} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
+import {message} from "ant-design-vue";
+import {Tool} from "../../util/tool";
 
 // const listData: any = [];
 
@@ -105,13 +73,39 @@ import axios from 'axios';
 
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+    // const ebooks1 = reactive({books: []});
+
+    const level1 = ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    }
 
     onMounted(() => {
-      axios.get( "/ebook/list",{
-        params:{
+      handleQueryCategory();
+      axios.get("/ebook/list", {
+        params: {
           page: 1,
           size: 1000
         }
@@ -124,10 +118,10 @@ export default defineComponent({
 
     return {
       ebooks,
-      ebooks2: toRef(ebooks1, "books"),
+      // ebooks2: toRef(ebooks1, "books"),
       // listData,
-      pagination : {
-        onChange: (page : any) => {
+      pagination: {
+        onChange: (page: any) => {
           console.log(page);
         },
         pageSize: 3,
@@ -137,17 +131,20 @@ export default defineComponent({
         {type: 'LikeOutlined', text: '156'},
         {type: 'MessageOutlined', text: '2'},
       ],
+
+      handleClick,
+      level1,
     }
   }
 });
 </script>
 
 <style scoped>
-  .ant-avatar{
-    width: 50px;
-    height: 50px;
-    line-height: 50px;
-    border-radius: 8%;
-    margin: 5px 0;
-  }
+.ant-avatar {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 8%;
+  margin: 5px 0;
+}
 </style>
