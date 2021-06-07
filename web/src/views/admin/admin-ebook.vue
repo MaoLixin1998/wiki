e4f2688d860a4d066111339f17d31c7757abbaa2
MaoLixin1998 <maoxin98116@163.com>
2021-06-07 13:54:36 +0800

    7-7 修改图书馆里页的bug，加载完分类后，在加载电子书，否则如果分类加载慢，电子书渲染会报错

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -238,9 +238,7 @@ export default defineComponent({
      * 查询所有分类
      **/
     const handleQueryCategory = () => {
-      loading.value = true;
       axios.get("/category/all").then((response) => {
-        loading.value = false;
         const data = response.data;
         if (data.success) {
           categorys = data.content;
@@ -249,6 +247,12 @@ export default defineComponent({
           level1.value = [];
           level1.value = Tool.array2Tree(categorys, 0);
           console.log("树形结构：", level1.value);
+
+          //加载完分类后，在加载电子书，否则如果分类加载慢，电子书渲染会报错
+          handleQuery({
+            page: 1,
+            size: pagination.value.pageSize,
+          });
         } else {
           message.error(data.message);
         }
@@ -267,10 +271,6 @@ export default defineComponent({
 
     onMounted(() => {
       handleQueryCategory();
-      handleQuery({
-        page: 1,
-        size: pagination.value.pageSize,
-      });
     });
 
     return {
