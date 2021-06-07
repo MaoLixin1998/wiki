453e03b2fe5ed33b838b6c04b3df48fe2d994b6e
MaoLixin1998 <maoxin98116@163.com>
2021-06-07 14:40:19 +0800

    7-8 默认显示欢迎页面，点击欢迎菜单时，显示欢迎页面，点击分类菜单时显示电子书，只有耳机分类才能触发click事件

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,10 +7,8 @@
           @click="handleClick"
       >
         <a-menu-item key="welcome">
-          <router-link :to="'/'">
-            <MailOutlined/>
-            <span>欢迎</span>
-          </router-link>
+          <MailOutlined/>
+          <span>欢迎</span>
         </a-menu-item>
         <a-sub-menu v-for="item in level1" :key="item.id">
           <template v-slot:title>
@@ -18,7 +16,7 @@
           </template>
           <a-menu-item v-for="child in item.children" :key="child.id">
             <MailOutlined/>
-            <span>{{child.name}}</span>
+            <span>{{ child.name }}</span>
           </a-menu-item>
         </a-sub-menu>
       </a-menu>
@@ -26,7 +24,11 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+      <div class="welcome" v-show="isShowWelcome">
+        <h1>欢迎使用利鑫wiki知识库</h1>
+      </div>
+      <a-list v-show="!isShowWelcome" item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }"
+              :data-source="ebooks">
         <template #renderItem="{ item }">
           <a-list-item key="item.name">
             <template #actions>
@@ -98,9 +100,12 @@ export default defineComponent({
       });
     };
 
-    const handleClick = () => {
-      console.log("menu click")
-    }
+    const isShowWelcome = ref(true);
+
+    const handleClick = (vulue:any) => {
+      // console.log(vulue)
+      isShowWelcome.value = vulue.key === 'welcome';
+    };
 
     onMounted(() => {
       handleQueryCategory();
@@ -134,6 +139,8 @@ export default defineComponent({
 
       handleClick,
       level1,
+
+      isShowWelcome
     }
   }
 });
