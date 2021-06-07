943ff8d420dabc7c6fe0aa95a383f00af28046d1
MaoLixin1998 <maoxin98116@163.com>
2021-06-07 15:55:33 +0800

    8-4 使用递归算法，将树形数据中，当前目标节点及其子孙节点设置成disabled 解决编辑保存后，重新编辑时，数据不更新的问题 为树形选择增加一个“无”选择，用来新增一级节点

--- web/src/views/admin/admin-category.vue
+++ web/src/views/admin/admin-category.vue
@@ -68,7 +68,7 @@
             无
           </a-select-option>
           <a-select-option v-for="c in level1" :key="c.id" :value="c.id" :disabled="category.id === c.id">
-            {{c.name}}
+            {{ c.name }}
           </a-select-option>
         </a-select>
       </a-form-item>
@@ -134,6 +134,8 @@ export default defineComponent({
      **/
     const handleQuery = () => {
       loading.value = true;
+      //如果不清空现有数据，则编辑保存重新加载数据后，再点编辑,则显示原数据
+      level1.value = [];
       axios.get("/category/all").then((response) => {
         loading.value = false;
         const data = response.data;
