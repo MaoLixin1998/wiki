ed0247ffcfc689e315d79d9efc5aed0dfe46b685
MaoLixin1998 <maoxin98116@163.com>
2021-06-07 16:39:54 +0800

    8-5 增加页面参数传递，完成新增文档功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -82,10 +82,12 @@ import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from "../../../util/tool";
+import {useRoute} from "vue-router";
 
 export default defineComponent({
   name: 'AdminDoc',
   setup() {
+    const route = useRoute();
     const param = ref();
     param.value = {};
     const docs = ref();
@@ -217,10 +219,10 @@ export default defineComponent({
 
       //不能选择当前节点及其所有子孙节点
       treeSelectData.value = Tool.copy(level1.value);
-      setDisable(treeSelectData.value,record.id);
+      setDisable(treeSelectData.value, record.id);
 
       //为选择树添加一个“无“
-      treeSelectData.value.unshift({id:0,name:'无'});
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
@@ -228,12 +230,13 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true;
-      doc.value = {};
-
+      doc.value = {
+        ebookId: route.query.ebookId
+      };
       treeSelectData.value = Tool.copy(level1.value);
 
       //为选择树添加一个“无“
-      treeSelectData.value.unshift({id:0,name:'无'});
+      treeSelectData.value.unshift({id: 0, name: '无'});
     };
 
     /**
