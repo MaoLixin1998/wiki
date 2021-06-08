95ece896ea2b5638a37005b6d83982a2363790b0
MaoLixin1998 <maoxin98116@163.com>
2021-06-08 09:44:23 +0800

    9-3 完成用户管理基本增删改查功能(一点小bug)

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,6 +2,7 @@ import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
 import Doc from '../views/doc.vue'
+import AdminUser from '../views/admin/admin-user.vue'
 import AdminEbook from '../views/admin/admin-ebook.vue'
 import AdminCategory from '../views/admin/admin-category.vue'
 import AdminDoc from '../views/admin/admin-doc.vue'
@@ -34,7 +35,7 @@ const routes: Array<RouteRecordRaw> = [
     {
         path: '/admin/user',
         name: 'AdminUser',
-        component: AdminEbook
+        component: AdminUser
     },
     {
         path: '/admin/doc',
