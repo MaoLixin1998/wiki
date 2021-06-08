30f8dded5dd087e812bea0feb4b3c6353bb8cbdf
MaoLixin1998 <maoxin98116@163.com>
2021-06-08 14:20:58 +0800

    9-11 增加了退出登录功能，退出登陆时，前端清除store登录信息，后端清除redis登录信息

--- src/main/java/com/lixin/wiki/controller/UserController.java
+++ src/main/java/com/lixin/wiki/controller/UserController.java
@@ -84,4 +84,11 @@ public class UserController {
         userService.delete(id);
         return resp;
     }
+    @GetMapping("/logout/{token}")
+    public CommonResp logout(@PathVariable String token) {
+        CommonResp resp = new CommonResp<>();
+        redisTemplate.delete(token);
+        LOG.info("从redis中删除token:{}",token);
+        return resp;
+    }
 }
--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -21,6 +21,16 @@
       <a-menu-item key="/about">
         <router-link to="/about">关于我们</router-link>
       </a-menu-item>
+      <a-popconfirm
+          title="确认退出登录?"
+          ok-text="是"
+          cancel-text="否"
+          @confirm="logout()"
+      >
+        <a class="login-menu" v-show="user.id">
+          <span>退出登录</span>
+        </a>
+      </a-popconfirm>
       <a class="login-menu" v-show="user.id">
         <span>您好：{{ user.name }}</span>
       </a>
@@ -83,7 +93,21 @@ export default defineComponent({
         if (data.success) {
           loginModalVisible.value = false;
           message.success("登陆成功！");
-          store.commit("setUser",user.value);
+          store.commit("setUser", data.content);
+        } else {
+          message.success(data.message);
+        }
+      });
+    };
+
+    // 登录
+    const logout = () => {
+      console.log("退出登录")
+      axios.get('/user/logout/'+ user.value.token ).then((response) => {
+        const data = response.data;
+        if (data.success) {
+          message.success("退出登陆成功！");
+          store.commit("setUser", {});
         } else {
           message.success(data.message);
         }
@@ -96,7 +120,8 @@ export default defineComponent({
       showLoginModal,
       loginUser,
       login,
-      user
+      logout,
+      user,
     }
   }
 });
@@ -106,5 +131,6 @@ export default defineComponent({
 .login-menu {
   float: right;
   color: white;
+  padding-left: 10px;
 }
 </style>
