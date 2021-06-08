14fea70531be98d711685cfe52e78ee84d211189
maolixin <maoxin98116@163.com>
2021-06-08 22:09:28 +0800

    10-9 将业务线程流水号传递给异步线程，方便生产运维查询

--- src/main/java/com/lixin/wiki/service/WsService.java
+++ src/main/java/com/lixin/wiki/service/WsService.java
@@ -1,6 +1,7 @@
 package com.lixin.wiki.service;
 
 import com.lixin.wiki.websocket.WebSocketServer;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
@@ -15,8 +16,8 @@ public class WsService {
     @Resource
     private WebSocketServer webSocketServer;
     @Async
-    public void sendInfo(String message) {
-
+    public void sendInfo(String message,String logId) {
+        MDC.put("LOG_ID",logId);
         webSocketServer.sendInfo(message);
     }
 
